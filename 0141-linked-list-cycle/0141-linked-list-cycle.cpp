ac1d1df693a3d7ab60a9be137b1f77254c9d2e9b
Anurag Garg <60929875+anurag-garg01@users.noreply.github.com>
2023-03-10 00:41:56 +0530

    Time: 7 ms (95.85%), Space: 8.1 MB (49.08%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,43 +1,47 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode(int x) : val(x), next(NULL) {}
+ *};
  */
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        // return whether linked list has cycle or not 
-        
-        if(head ==NULL)
-            return false;
-            if(head->next ==NULL){
+class Solution
+{
+    public:
+        bool hasCycle(ListNode *head)
+        {
+           	// return whether linked list has cycle or not 
+
+            if (head == NULL)
+                return false;
+            
+            if (head->next == NULL)
+            {
                 false;
             }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(slow!=NULL ){
-            fast = fast->next;
-            if(fast!=NULL){
-                fast=fast->next;
-            }
-            else{
-                return false;
-            }
             
-            slow = slow->next;
-            if(fast==NULL)
-                return false;
-            if(fast==slow)
-                return true;
+            ListNode *slow = head;
+            ListNode *fast = head;
+
+            while (slow != NULL)
+            {
+                fast = fast->next;
+                if (fast != NULL)
+                {
+                    fast = fast->next;
+                }
+                
+                if (fast == NULL)
+                    return false;
+
+                slow = slow->next;
+               
+                if (fast == slow)
+                    return true;
+            }
+
+            return false;
             
         }
-        
-        
-        
-        return false;
-    }
 };
