8710525f7afa91f72f3fe9d365a7e5d128de9685
Anurag Garg <60929875+anurag-garg01@users.noreply.github.com>
2023-03-21 17:56:19 +0530

    Time: 154 ms (93.67%), Space: 107.4 MB (97.44%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        /// return the number of arrays filled with 0s
+        
+        long long int cnt =0;
+        long long int sum =0;
+        long long int n = nums.size();
+        
+        for(int i =0;i<n;i++){
+            if(nums[i]==0){
+                cnt++;
+                
+            }
+            else{
+                sum +=  cnt*(cnt+1)/2;
+                cnt =0;
+            }
+        
+        }
+        
+        return max(sum,sum+cnt*(cnt+1)/2);
+    }
+};
