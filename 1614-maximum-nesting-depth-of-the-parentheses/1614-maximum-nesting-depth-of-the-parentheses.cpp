90f4d9a4fc9eeff67a47b647d26f2fec74babeb8
Anurag Garg <60929875+anurag-garg01@users.noreply.github.com>
2023-03-24 18:09:33 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (91.98%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,30 +1,41 @@
-class Solution {
-public:
-    int maxDepth(string s) {
-        // what is the max nesting that has been done to store the element 
-        
-        //stack could be used 
-        
-        // return the max size of stack
-        int ans =0;
-        stack<char> st;
-        
-        for(int i = 0;i<s.size();i++){
-            char ch = s[i];
-            
-            if(ch=='('){
-                st.push(ch);
-               // ans = max(ans,st.size());
-                if(st.size()>=ans){
-                    ans = st.size();                }
+class Solution
+{
+    public:
+        int maxDepth(string s)
+        {
+           	// what is the max nesting that has been done to store the element 
+
+           	//stack could be used 
+
+           	// return the max size of stack
+           	// tc :o(n)   | sc:o(n)
+            int open = 0;
+            int ans = 0;
+            stack<char> st;
+
+            for (int i = 0; i < s.size(); i++)
+            {
+                char ch = s[i];
+
+                if (ch == '(')
+                {
+                   	// st.push(ch);
+                    open++;
+                   	// ans = max(ans,st.size());
+                    // if (st.size() >= ans)
+                    // {
+                    //     ans = st.size();
+                    // }
+                    if(open>=ans)
+                    ans = open; 
+                }
+                else if (ch == ')')
+                {
+                    open--;
+                }
             }
-            
-            else if(ch == ')'){
-                st.pop();
-            }     
-       }
+
+            return ans;
         
-        
-        return ans;
-    }
+        }
 };
