b8fbb3b138301568e4aee193bef72d4af536032a
Anurag Garg <60929875+anurag-garg01@users.noreply.github.com>
2023-04-23 12:26:39 +0530

    Time: 2803 ms (5.06%), Space: 39.2 MB (87.67%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.cpp
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> fairCandySwap(vector<int>& a, vector<int>& b) {
         // tc : o(n)  sc: o(n)
         vector<int> ans(2);
+       // int n = a.size();
         
         sort(a.begin(),a.end());
         sort(b.begin(),b.end());
@@ -20,17 +21,29 @@ public:
             s2 += y; 
         }
         
-      set<int> s;
         
-        for(auto k : b){
-            s.insert(k);
-        }
-        
-        for(int i =0;i<a.size();i++){
-            if(s.find(a[i]+(s2-s1)/2)!=s.end()){
-                return {a[i],a[i]+(s2-s1)/2};
+        for(int i=0;i<a.size();i++){
+            for(int j=0;j<b.size();j++){
+                if(a[i]+(s2-s1)/2 ==b[j] ){
+                    return{a[i],a[i]+(s2-s1)/2};
+                }
             }
         }
+        
+        
+        
+        
+//       set<int> s;
+        
+//         for(auto k : b){
+//             s.insert(k);
+//         }
+        
+//         for(int i =0;i<a.size();i++){
+//             if(s.find(a[i]+(s2-s1)/2)!=s.end()){
+//                 return {a[i],a[i]+(s2-s1)/2};
+//             }
+//         }
         
         return ans;
         
