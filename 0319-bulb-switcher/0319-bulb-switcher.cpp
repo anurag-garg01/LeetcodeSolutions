56c86eec14c764a5490a541b38efc837cc098c91
Anurag Garg <60929875+anurag-garg01@users.noreply.github.com>
2023-04-27 17:25:32 +0530

    Time: 3 ms (45.34%), Space: 6.1 MB (9.91%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        // return the number of bulbs that are on after n rounds 
+        
+        
+        // only o(n) solution permissible   
+        // mathematics based solution
+        return sqrt(n);
+        
+    }
+};
