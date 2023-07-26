b29c93edafd93a9949035062f3cae7951ae19a91
Anurag Garg <garganurag3955@gmail.com>
2023-07-26 12:14:03 +0530

    Time: 24 ms (91.32%), Space: 22.4 MB (29.63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-            int low = 0;
-            int high = nums.size()-1;
+        // even odd concept will be applied 
+
+        int l = 0;
+        int h = nums.size()-1;
         int n = nums.size();
-        if(nums.size()==1)
+        if(n==1)
+        return nums[0];
+        // base case
+        if(nums[0]!=nums[1]){
             return nums[0];
-        
-        if(nums[0]!=nums[1])
-            return nums[0];
-        
-        if(nums[n-1]!=nums[n-2]){
+        }
+
+
+        if(nums[n-2]!=nums[n-1])
             return nums[n-1];
-        }
-        
-        while(low<=high){
-            // thought of right logic 
-            int mid = low  + (high-low)/2;
-            
-            // check edge cases
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
+
+        while(l<=h){
+            int mid = l+(h-l)/2;
+
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
                 return nums[mid];
-            
-            if( mid%2==1 && nums[mid]==nums[mid+1])
-                high = mid-1;
-            else if(mid%2==1 && nums[mid]==nums[mid-1]){
-                low = mid + 1;
             }
-            
-            if(mid%2==0 && nums[mid]==nums[mid+1]){
-                low = mid+1;
+
+            else if(mid%2 ==0 && nums[mid]!=nums[mid+1] || (mid%2==1 && nums[mid]==nums[mid+1])){
+                h=mid-1;
             }
-            else if(mid%2==0 && nums[mid]==nums[mid-1]){
-                high = mid-1;
+            else{
+                l = mid +1;
             }
-            
-            
         }
+
+
         return -1;
-        
+
     }
 };
