3d586e009669d89eb1a68e3b4678fa1c3bee80ce
Anurag Garg <garganurag3955@gmail.com>
2023-08-06 21:13:25 +0530

    Time: 124 ms (88.56%), Space: 67.9 MB (29.22%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,34 +1,57 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-
-
-
-        sort(nums.begin(),nums.end());
-
+        
+        // this can be solved using tc : o(n)  sc :o(n)
         int n = nums.size();
-        int i =0;
-        int j = n-1;
-        int cnt =0;
 
-        while(i<j){
-            if(nums[i]+nums[j]==k){
-                cnt++;
-                i++;
-                j--;
-            }
+        unordered_map<int,int> mp;
+        int count =0;
 
-            else if(nums[i]+nums[j]<k){
-                i++;
+        for(int i =0;i<n;i++)
+        {
+            if(mp.find(k-nums[i])!=mp.end()){
+                count++;
+                mp[k-nums[i]]--;
+                // remove the element from map if occurence is zero 
+                if(mp[k-nums[i]]==0){
+                    mp.erase(k-nums[i]);
+                }
             }
             else{
-                j--;
+                mp[nums[i]]++;
             }
         }
 
+        return count;
+    //     // tc : o(nlogn) sc : o(1)
+    //     // two pointers method used
+
+    //     sort(nums.begin(),nums.end());
+
+    //     int n = nums.size();
+    //     int i =0;
+    //     int j = n-1;
+    //     int cnt =0;
+
+    //     while(i<j){
+    //         if(nums[i]+nums[j]==k){
+    //             cnt++;
+    //             i++;
+    //             j--;
+    //         }
+
+    //         else if(nums[i]+nums[j]<k){
+    //             i++;
+    //         }
+    //         else{
+    //             j--;
+    //         }
+    //     }
 
 
 
-        return cnt;
+
+    //     return cnt;
     }
 };
