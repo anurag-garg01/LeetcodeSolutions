ec77f8710871d8898af7792a8ae3b48b2f12ed0e
Anurag Garg <garganurag3955@gmail.com>
2023-09-09 09:43:38 +0530

    Time: 6 ms (53.32%), Space: 20.7 MB (30.95%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,48 @@
-class Solution {
-public:
-    string reverseWords(string str) {
-        // the sky is blue => blue is the sky 
+class Solution
+{
+    public:
+        string reverseWords(string s)
+        {
 
+           	// here's the o(1) space solution without sstream 
 
-        stringstream s(str);
+            string res;
+            int i = 0;
+            int j = 0;
+            int n = s.size();
 
-        string word;
-        
-        string ans ;
+            while (i < n)
+            {
 
-        while(s>>word){
-            ans = word + " " + ans;
+                while (i < n && s[i] == ' ')
+                {
+                    i++;
+                }
+
+                if (i == n)
+                    return res;
+
+                j = i + 1;
+
+                while (j < n and s[j] != ' ')
+                {
+                    j++;
+                }
+
+                string temp = s.substr(i, j - i);
+
+                if (res.size() == 0)
+                {
+                    res = temp;
+                }
+                else
+                {
+                    res = temp + " " + res;
+                }
+
+                i = j + 1;
+            }
+
+            return res;
         }
-
-        //reverse(ans.begin(),ans.end());
-        // ans = ans - " ";    
-        
-        int n = ans.size();
-
-        return ans.substr(0,n-1);
-
-    }
 };
