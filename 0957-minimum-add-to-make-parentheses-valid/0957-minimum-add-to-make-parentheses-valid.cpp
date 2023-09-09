f876e39e44c8bbd78e0cdb636366a23d03a90faf
Anurag Garg <garganurag3955@gmail.com>
2023-09-09 10:17:11 +0530

    Time: 0 ms (100%), Space: 6.2 MB (69.76%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,33 +1,62 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        // Minimum addittions to make the parenthestis alid again 
-
-        // stack 
 
 
-        stack<int> st;
-        int ans = 0;
-        int n = s.size();
-        for(int i =0;i<n;i++){
-            // if(s[i]=='(')
-            //     st.push('(');
+       //  solving without any extra space and in O(1) space
+       // could it be done in o(1) space ???
+       int left =0;
+       int right = 0;
+
+       for(char ch : s){
+           if(ch=='(')
+            right++;
+            else if(ch==')' && right>0){
+                right--;
+       }
+       else {
+           left++;
+       }
+
+
+    
+       }
+
+       return left + right;
+
+
+
+
+
+
+
+
+
+        // // Minimum addittions to make the parenthestis alid again 
+
+        // // stack used and solved in O(n)
+
+
+
+        // stack<int> st;
+        // int ans = 0;
+        // int n = s.size();
+
+        // for(int i =0;i<n;i++){
+        //     // if(s[i]=='(')
+        //     //     st.push('(');
             
-            if(s[i]==')' && !st.empty() && st.top()=='('){
-                st.pop();
-            }
-            else {
-                st.push(s[i]);
-            }
-        }
+        //     if(s[i]==')' && !st.empty() && st.top()=='('){
+        //         st.pop();
+        //     }
+        //     else {
+        //         st.push(s[i]);
+        //     }
+        // }
 
+        // ans = st.size();
 
-
-        ans = st.size();
-
-
-
-        return ans;
+        // return ans;
 
 
 
