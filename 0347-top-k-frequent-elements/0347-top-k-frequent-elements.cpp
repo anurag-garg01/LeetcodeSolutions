18d6a2f8995f0889785e24f5aa7873fe62702235
Anurag Garg <garganurag3955@gmail.com>
2025-03-17 09:52:53 +0530

    Time: 3 ms (52.16%), Space: 19.7 MB (16.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,41 +1,62 @@
-class Solution
-{
-    public:
-        vector<int> topKFrequent(vector<int> &nums, int k)
-        {
-           	// solving using bucket sort 
-            vector<int> ans;
-           	// create a map 
-            unordered_map<int, int> mp;
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        /*
 
-            for (auto x: nums)
-            {
-                mp[x]++;
-            }
+        1-3
+        2-2 
+        3-1
 
-            vector<vector < int>> buckets(nums.size() + 1);
-            for (auto[a, b]: mp)
-            {
-                buckets[b].push_back(a);
-            }
+        o to n arry 
 
-           	// last step is to get elements from buckets 
 
-            for (int i = nums.size(); i >= 0; i--)
-            {
-                if (buckets[i].size() != 0)
-                {
-                    for (auto t: buckets[i])
-                    {
-                        if (k != 0)
-                        {
-                            ans.push_back(t);
+        0 1 2 3 4 5 
+        |
+    
+
+        */
+
+        vector<int> ans;
+        int n = nums.size();
+        
+        vector<vector<int>> arr(n+1);
+
+
+        unordered_map<int,int> mp;
+
+        for(auto x : nums){
+            mp[x]++;
+        }
+
+
+        for(auto &[key,val] :mp){
+            arr[val].push_back(key);
+        }
+
+
+        // traverse through array 
+
+
+        for(int i =n;i>=0;i--){
+           if(k>0){
+                if(arr[i].size()!=0){
+                    for(auto n : arr[i]){
+                        if(k>0){
+                            ans.push_back(n);
                             k--;
                         }
                     }
                 }
-            }
-            
-            return ans;
+           }
         }
+
+
+
+
+
+
+        return ans;
+
+
+    }
 };
