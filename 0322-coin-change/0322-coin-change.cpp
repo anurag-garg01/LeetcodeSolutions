52b772b25a3359010a366f97d808c1258f944aea
Anurag Garg <garganurag3955@gmail.com>
2025-03-19 22:17:15 +0530

    Time: 2416 ms (5.12%), Space: 598.4 MB (5.09%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int func(vector<int> nums, int amount, int ind, vector<vector<int>>& dp) {
+        // base case
+
+        if (ind == 0) {
+            if (amount % nums[0] == 0) {
+                return amount / nums[0];
+            } else {
+                return 1e9;
+            }
+        }
+
+        // take and not take condition
+
+        if (dp[ind][amount] != -1) {
+            return dp[ind][amount];
+        }
+
+        // not_take
+        int not_take = func(nums, amount, ind - 1,dp);
+
+        // take condn
+        int take = 1e9;
+        if (nums[ind] <= amount) {
+            take = 1 + func(nums, amount - nums[ind], ind,dp);
+        }
+
+        return dp[ind][amount] = min(take, not_take);
+    }
+
+    int coinChange(vector<int>& nums, int amount) {
+        // return the fewest amount of coins required
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        int ans = func(nums, amount, n - 1, dp);
+
+        if (ans >= 1e9)
+            return -1;
+
+        return ans; // Return the minimum number of elements needed
+    }
+};
