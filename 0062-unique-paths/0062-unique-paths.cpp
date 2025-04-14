c0b7288e5b1caf0005d5f8136d2495def5c13c0a
Anurag Garg <garganurag3955@gmail.com>
2025-04-14 13:16:26 +0530

    Time: 0 ms (100%), Space: 9.4 MB (9.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,25 +1,37 @@
 class Solution {
 public:
-    int grid(int m , int n , int i , int j,vector<vector<int>> &dp){
-        
-        if(i==m-1 && j==n-1){
+    int func(int m, int n,vector<vector<int>> dp){
+        if(m<0 || n<0){
+            // out of bounds condn
+            return 0;
+        }
+
+
+        if(m==0 && n==0){
+            // reached the target
             return 1;
         }
-        
-        if(i>=m || j>=n)
-            return 0;
-        
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        
-        return dp[i][j]= grid(m,n,i+1,j,dp) + grid(m,n,i,j+1,dp);
-        
+
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+
+        return dp[m][n]= func(m-1,n,dp) + func(m,n-1,dp);
+
     }
-    int uniquePaths(int m , int n){
-       vector<vector<int>> dp(m,vector<int>(n,-1));
-      //  memset(dp,-1,sizeof(dp));
-        return grid(m,n,0,0,dp);
-        
+    int uniquePaths(int m, int n) {
+        // return the total number of unique paths 
+
+        vector<vector<int>> dp(m,vector<int>(n,1));
+
+        for(int i =1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+           }
+        }
+
+        return dp[m-1][n-1];
+
+        // return func(m-1,n-1,dp);
     }
-    
 };
