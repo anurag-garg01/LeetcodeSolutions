651258df2abbdaf5b676842e26d1e591ea6ba318
Anurag Garg <garganurag3955@gmail.com>
2025-04-14 15:27:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-    
-    int recursive(vector<vector<int>> &grid,int i,int j,vector<vector<int>> &dp)
-    {
-        // base case 
-        if(i==0 && j==0){
-            // we have reached the base case 
-            return grid[i][j];
-        }
-        // here's we get the out of bound conditions 
-        if(i<0 || j<0 )
-            // here we cannot use INT_MAX coz below in the code we will have                    something + to int_max that will give overflow 
+    int func(vector<vector<int>> grid, int m , int n,vector<vector<int>> &dp){
+        // out of bounds scenario 
+        if(m<0 || n<0){
             return 1e9;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
         }
-       int up = grid[i][j] + recursive(grid,i-1,j,dp);
-        int left = grid[i][j] +recursive(grid,i,j-1,dp);
-            
-            return dp[i][j] = min(up,left);
+
+        // base case 
+        if(m==0 && n==0){
+            return grid[m][n];
+        }
+
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+
+
+
+        // take and not_take condn 
+        int left = 1e9;
+        if(n-1>=0){
+         left = grid[m][n] + func(grid,m,n-1,dp);
+        }
+        int up = 1e9;
+        // not_take 
+        if(m-1>=0){
+         up  = grid[m][n] + func(grid,m-1,n,dp);
+        }
+
+
+        return dp[m][n] = min(left,up);
+
+
     }
-    
     int minPathSum(vector<vector<int>>& grid) {
-        // path from top left to bottom right suhc that the sum of number is              minimum
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-      //  int dp[n+1][m+1];
-        //memset(dp,-1,sizeof(dp));
-        return recursive(grid,n-1,m-1,dp);
+        // return the maximum path sum 
+
+        // take or not_take 
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+
+        
+        return func(grid, m-1,n-1,dp);
+
     }
 };
