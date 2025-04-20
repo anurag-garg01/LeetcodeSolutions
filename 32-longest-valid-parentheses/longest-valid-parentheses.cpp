cdc456f5d4ab5567f36bfc8951037b7e9d40d75e
Anurag Garg <garganurag3955@gmail.com>
2025-04-20 14:00:27 +0530

    Time: 1 ms (39.25%) | Memory: 10.3 MB (58.09%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,60 +1,34 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        // return the longest valid parentheses in string s 
-        int left =0;
-        int right =0;
+        int ans = 0;
 
-        int maxi =0;
+        // [-1,0,1]
+        stack<int> st;
+        int maxi = 0;
 
+        // add an element -1 in the stack in the beginning
 
+        st.push(-1);
 
         int n = s.size();
 
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                st.pop();
 
-        for(int i =0;i<n;i++){
-            if(s[i]=='('){
-                left++;
-            }
-            else {
-                right++;
-            }
-
-            if(left==right){
-                maxi = max(maxi,left*2);
-            }
-            else if(right>left){
-                left =0;
-                right =0;
-            }
-        }
-
-
-        left =0;
-        right =0;
-
-
-
-        for(int i =n-1;i>=0;i--){
-            if(s[i]=='('){
-                left++;
-            }
-            else {
-                right++;
-            }
-
-            if(left==right){
-                maxi = max(maxi,right*2);
-            }
-            else if(left>right){
-                left =0;
-                right =0;
+                if (st.empty()) {
+                    st.push(i);
+                } else {
+                    // stack is not empty
+                    // take the max value
+                    maxi = max(maxi, (i - st.top()));
+                }
             }
         }
 
         return maxi;
-
-
-        
     }
 };
