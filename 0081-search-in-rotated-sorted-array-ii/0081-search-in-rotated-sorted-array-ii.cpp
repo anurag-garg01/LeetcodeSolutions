7c2161f06b653ed1bb693d8b8feb1275455b5194
Anurag Garg <garganurag3955@gmail.com>
2025-05-16 13:22:53 +0530

    Time: 8 ms (1.02%), Space: 14.4 MB (100%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,44 +1,72 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        // return true if target element is present 
-        // coding 
+        // core : 
+
+       // search in rotated sorted array 2 
+
+       // just that the numbers are repeated now 
+
+        
+        // hamdling edge case 
+        if(target == nums[0]){
+            return true;
+        }
+        if(target==nums[nums.size()-1]){
+            return true;
+        }
         int low = 0;
         int high = nums.size()-1;
         
+
         while(low<=high){
+             if(target == nums[low]){
+            return low;
+        }
+        if(target==nums[high]){
+            return high;
+        }
             int mid = low + (high-low)/2;
-            
+            cout<<mid;
             if(nums[mid]==target){
                 return true;
             }
-            else if(nums[low]<nums[mid]){
-                // left part is sorted
-                if(target<nums[low] || target >nums[mid]){
-                    // does not belong to the left part 
+
+            // if(nums[low]<=nums[mid] && (nums[low]<=target && target <= nums[mid])){
+            //     high = mid-1;
+            // }
+            // else {
+            //     low = mid+1;
+            // }
+
+            // either left side sorted or right side is sorted 
+            // if left side is sorted 
+            if(nums[low]<nums[mid]){
+                if(nums[low]<target && target < nums[mid]){
+                    high = mid-1;
+                }
+                else {
+                    low = mid+1;
+                }
+            }
+            else if(nums[mid]<nums[high]){
+                // if right side is sorted
+
+                if(target>nums[mid] && target <nums[high]){
                     low = mid+1;
                 }
                 else {
-                    high = mid-1;
-                }
-                
-            }
-            else if(nums[low]>nums[mid]){
-                if(target<nums[mid] || target> nums[high]){
-                    // target does not belong to the right part 
-                    high = mid-1;
-                }
-                else{
-                    
-                    low = mid+1;
+                    high = mid -1;
                 }
             }
-            else{
-                 // duplicates
+            else {
+                // remove duplicates
                 low++;
+
             }
-        }
-        
+            
+        } 
+
         return false;
     }
 };
