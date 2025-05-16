271ef26c72cc2093de0db02d3554f252b1d9994c
Anurag Garg <garganurag3955@gmail.com>
2025-05-16 13:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (32.11%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,55 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        # search in rotated sorted array 2 , that can have duplicates 
+
+        # search in rotated sorted array 
+
+         # search in the rotated sorted array 
+
+        low =0
+        n = len(nums)
+        high = n-1
+
+
+        if nums[low]==target:
+            return True
+
+
+        if nums[high]==target:
+            return True
+
+        while(low<=high):
+            mid = (low + high)//2      # 3, 5
+
+            if (nums[mid]==target):
+                return True
+
+            if nums[low]==target:
+                return True
+
+
+            if nums[high]==target:
+                return True
+            
+            # check the left sorted 
+
+            if(nums[low]<nums[mid]):
+                # left sorted 
+                if (nums[low]<=target and target <=nums[mid]):
+                    high = mid-1
+                else:
+                    low = mid+1
+            
+            elif (nums[mid]<nums[high]) :
+                # right sorted 
+                if(nums[mid]<=target and target<=nums[high]):
+                    low = mid+1
+                else :
+                    high = mid-1
+            else : 
+                low+=1
+
+        return False
+
+
+
