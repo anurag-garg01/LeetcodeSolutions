e887842090ede64e24a7c01b2ffb526be0af04d0
Anurag Garg <garganurag3955@gmail.com>
2025-05-21 01:15:03 +0530

    Time: 0 ms (100%), Space: 11 MB (99.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        // nums 1 has m + n elements so consider that and code
+        // merge the two arrays 
+
+        //find the last index ->
+
+
+        int last = m+n-1;
+
+
+        while(m>0 && n>0){
+
+            if(nums1[m-1]>nums2[n-1]){
+                nums1[last]=nums1[m-1];
+                m--;
+            }
+            else {
+                nums1[last]=nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+
+
+        // if n>0 left 
+
+        while(n>0){
+            nums1[last]=nums2[n-1];
+            n--;
+            last--;
+
+        }
         
-        int i = m-1;
-        int j= n-1;
-        int k = m+n;
-        if(m==0){
-            for(int i =0;i<nums1.size();i++){
-                nums1[i]=nums2[i];
-            }
-        }
-        if(n!=0){
-        while(i>=0 && j>=0){
-            // compare last two indices and operate
-            if(nums1[i]>=nums2[j]){
-                nums1[k-1] =  nums1[i];
-                k--;
-                i--;
-            }
-            else{
-                nums1[k-1]= nums2[j];
-                j--;
-                k--;
-            }
-            }
-            
-            if(i<0 && j>=0){
-                while(k>=0 && j>=0){
-                nums1[k-1]=nums2[j];
-                k--;
-                j--;
-                }
-            }
-            if(j<0 && i>=0){
-                while(k>=0 && i>=0){
-                nums1[k-1]=nums1[i];
-                k--;
-                i--;
-                }
-            }
-        }
     }
 };
