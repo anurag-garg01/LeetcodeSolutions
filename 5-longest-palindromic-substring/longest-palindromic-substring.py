d29a3ce750246f5a87b56daf8110dfa37a81f306
Anurag Garg <garganurag3955@gmail.com>
2025-05-24 17:27:40 +0530

    Time: 523 ms (40.18%) | Memory: 18 MB (28.46%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,73 +1,36 @@
 class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # return the longest palindroming substring 
 
-    def check_palindrome(self, s : str,i : int , j : int) -> str:
-        n = len(s)
+        ans_len = 0
         ans = ""
+        n = len(s)
+        
+        # def is_palindrome(i,j):
+        #     return true/false 
+        
+        def check_palindrome(i,j):
+            nonlocal ans_len
+            nonlocal ans
 
-        while(i>=0 and j<n):
-            if (i>=0 and s[i]==s[j] and j<n):
-                ans = s[i:j+1]
+            while i>=0 and j<n and s[i]==s[j]:
+                if((j-i+1)>=ans_len):
+                    ans = s[i:j+1]
+                ans_len = max(ans_len, (j-i+1))
                 i-=1
                 j+=1
-            else:
-                break
-        
-
-        return ans
-                
-
-            
-        
-        # break - out of loop 
-
-        
-
-    def longestPalindrome(self, s: str) -> str:
-        # return the longest palindrome in a string 
-
-        # "babad"
-
-        start = 0
-        end = len(s)
-        n = len(s)
-        ans = ""
-        maxi = 0
-
-        mid = (start+end)//2
-
-
-        for i in range(n):
-            # for odd case 
-            res = self.check_palindrome(s,i,i)
-
-            print(f"i = {i} and res = {res}")
-
-            if len(res)>maxi:
-                maxi = len(res)
-                ans = res
-
-        # now for even case
-
-
-        for i in range(n):
-            # for odd case 
-            res = self.check_palindrome(s,i,i+1)
-
-            print(f"i = {i} and res = {res}")
-
-            if len(res)>maxi:
-                maxi = len(res)
-                ans = res
-        
-
-
-
-        return ans
-
             
 
 
+        for i in range (len(s)):
 
+            check_palindrome(i,i)
 
+            check_palindrome(i,i+1)
 
         
+
+        return ans
+
+
+
