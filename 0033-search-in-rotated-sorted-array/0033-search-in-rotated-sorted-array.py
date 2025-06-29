be502ea5acf6de71c933d9433ec0e30801ebaf4a
Anurag Garg <garganurag3955@gmail.com>
2025-06-29 22:03:38 +0530

    Time: 0 ms (100%), Space: 18.1 MB (66.62%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,31 +1,39 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # search in the rotated sorted array 
 
-        low =0
+        # hust check if the left side is sorted or the right side is sorted 
+
         n = len(nums)
+
+        if target == nums[0]: # checking first element 
+            return 0
+
+        if target == nums[-1]: # checking last element 
+            return n-1
+        
+        low = 0
         high = n-1
 
-        while(low<=high):
-            mid = (low + high)//2      # 3, 5
+        while low<=high:
+            mid = low + (high-low)//2
 
-            if (nums[mid]==target):
-                return mid
+            # check the left sorted and right sorted 
+            if nums[mid]==target:
+                return mid 
             
-            # check the left sorted 
-
-            if(nums[low]<=nums[mid]):
-                # left sorted 
-                if (nums[low]<=target and target <=nums[mid]):
+            elif nums[low]<=nums[mid]: # left sorted 
+                if target>=nums[low] and target <=nums[mid]:
                     high = mid-1
-                else:
-                    low = mid+1
-            
-            else :
-                # right sorted 
-                if(nums[mid]<=target and target<=nums[high]):
+                else :
+                    low = mid +1
+            else:
+                if target>=nums[mid] and target <=nums[high]:
                     low = mid+1
                 else :
                     high = mid-1
+            
+        
+        return -1
 
-        return -1 
+
+        
